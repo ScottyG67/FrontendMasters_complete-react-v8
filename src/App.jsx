b5ec9681f6dccdc5ca0f7255403e797de9188898
Scott Gloyna <scott.gloyna@bestow.com>
2023-02-06 15:28:24 -0600

    more fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
+import Pet from "./Pet"
 
 const App = () => {
-  <div>
-    <h1>Adopt ME!</h1>
-    <Pet name="Dory" animal="Dog" breed="Everything" />
-    <Pet name="Lilo" animal="Dog" breed="Aussi" />
-    <Pet name="Lacy" animal="Dog" breed="Lab" />
-  </div>;
+  return(
+    <div>
+      <h1>Adopt ME!</h1>
+      <Pet name="Dory" animal="Dog" breed="everything" />
+      <Pet name="Lilo" animal="Dog" breed="Aussie" />
+      <Pet name="Lacy" animal="Dog" breed="Lab" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
